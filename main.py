6f900c1d4ba806429a5434a79e30fbb9ca3a0bae
clodi <ciao@clodi.tech>
2023-11-29 17:16:27 +0100

    verify

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 HISTORY_FILE = 'history.csv'
 STATS_FILE = 'stats.csv'
 COMBO_FILE = 'combo.csv'
+PLAY_FILE = 'play.csv'
 
 def get_winning():
     # ask the user to enter the winning combination
@@ -19,9 +20,36 @@ def get_winning():
 
     return combo
 
+def verify(combo):
+    with open(PLAY_FILE, 'r') as f:
+        plays = f.readlines()
+    
+    # init matches counter from 1 to 6
+    matches = {i: 0 for i in range(1, 7)}
+
+    for play in plays:
+        print(f'\n>   play: {play}')
+        this = 0
+
+        # count the number of matches
+        for i, number in enumerate(play.split(',')):
+            if number in combo:
+                this += 1
+        
+        # increment the counter
+        if this > 0:
+            matches[this] += 1
+
+    return {k: v for k, v in matches.items() if v > 0}
+
 def main():
     # get the last winning combination
     win_combo = get_winning()
     print(f'>   winning combination: {win_combo}')
+
+    # verify if the winning combination has been played
+    print('>>  verifying if it has been played')
+    verification = verify(win_combo)
+    print(f'>   verification: {verification}')
 
 main()
