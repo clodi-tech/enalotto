071351c3effec8c5a60e39cd5dd52544cfe3f6af
clodi <ciao@clodi.tech>
2024-04-18 14:48:12 +0200

    random numbers

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
 export default function Home() {
+  const numbers = Array.from({length: 10}, () => Math.floor(Math.random() * 90) + 1);
+
   return (
     <main>
       <h1>Forecast</h1>
       <h2>top 10 for the next lottery</h2>
       <small>2024.04.20</small>
       <div className="flex flex-col justify-center items-center gap-2">
-        <small>01.</small><p>01</p>
-        <small>02.</small><p>02</p>
-        <small>03.</small><p>03</p>
-        <small>04.</small><p>04</p>
-        <small>05.</small><p>05</p>
-        <small>06.</small><p>06</p>
-        <small>07.</small><p>07</p>
-        <small>08.</small><p>08</p>
-        <small>09.</small><p>09</p>
-        <small>10.</small><p>10</p>
+        {numbers.map((number, index) => (
+          <div key={index} className="flex gap-2 justify-center items-center">
+            <small>{(index+1).toString().padStart(2, '0')}.</small>
+            <p>{number.toString().padStart(2, '0')}</p>
+          </div>
+        ))}
       </div>
     </main>
   )
