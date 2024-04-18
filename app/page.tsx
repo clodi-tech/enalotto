90b1f4b287172b1b1ff8ae700334bd5ee9a25b01
clodi <ciao@clodi.tech>
2024-04-18 15:01:29 +0200

    render top 10

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ export default function Home() {
     score: Math.floor(Math.random() * 101),
   }));
 
+  const topForecasts = forecasts.sort((a, b) => b.score - a.score).slice(0, 10);
+
   return (
     <main>
       <h1>Forecast</h1>
       <h2>top 10 for the next lottery</h2>
       <small>2024.04.20</small>
       <div className="flex flex-col justify-center items-center gap-2">
-        {forecasts.map((forecast, index) => (
+        {topForecasts.map((forecast, index) => (
           <div key={index} className="flex gap-2 justify-center items-center">
             <small>{(index+1).toString().padStart(2, '0')}.</small>
             <p>{forecast.number.toString().padStart(2, '0')}</p>
