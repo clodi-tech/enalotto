1e9190081453802c704c24401ea9308c3913e7ad
clodi <ciao@clodi.tech>
2024-04-18 15:05:13 +0200

    score max 99 mono font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { JetBrains_Mono } from 'next/font/google';
+
+const mono = JetBrains_Mono({ 
+    subsets: ["latin"],
+    weight: ['300']
+});
+
 export default function Home() {
   const forecasts = Array.from({length: 90}, (_, i) => ({
     number: i + 1,
-    score: Math.floor(Math.random() * 101),
+    score: Math.floor(Math.random() * 100),
   }));
 
   const topForecasts = forecasts.sort((a, b) => b.score - a.score).slice(0, 10);
@@ -13,10 +20,12 @@ export default function Home() {
       <small>2024.04.20</small>
       <div className="flex flex-col justify-center items-center gap-2">
         {topForecasts.map((forecast, index) => (
+          <div className={mono.className}>
           <div key={index} className="flex gap-2 justify-center items-center">
             <small>{(index+1).toString().padStart(2, '0')}.</small>
             <p>{forecast.number.toString().padStart(2, '0')}</p>
             <small className="px-1 border border-slate-700 rounded-sm">{forecast.score.toString().padStart(2, '0')}</small>
+          </div>
           </div>
         ))}
       </div>
