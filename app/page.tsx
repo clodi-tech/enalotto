4639ef9861d70bb54265c97cef437602bdf08071
clodi <ciao@clodi.tech>
2024-04-18 14:43:03 +0200

    forecast setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
     <main>
-      main
+      <h1>Forecast</h1>
+      <h2>top 10 for the next lottery</h2>
+      <small>2024.04.20</small>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <small>01.</small><p>01</p>
+        <small>02.</small><p>02</p>
+        <small>03.</small><p>03</p>
+        <small>04.</small><p>04</p>
+        <small>05.</small><p>05</p>
+        <small>06.</small><p>06</p>
+        <small>07.</small><p>07</p>
+        <small>08.</small><p>08</p>
+        <small>09.</small><p>09</p>
+        <small>10.</small><p>10</p>
+      </div>
     </main>
-  );
+  )
 }
