92f123d01c5c766ed2c32c5cf25acc08788007d3
clodi <ciao@clodi.tech>
2024-04-18 15:10:28 +0200

    re organise

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,10 @@ export default function Home() {
       <h1>Forecast</h1>
       <h2>top 10 for the next lottery</h2>
       <small>2024.04.20</small>
-      <div className="flex flex-col justify-center items-center gap-2">
+      <div className="flex justify-center items-center gap-4 max-w-sm flex-wrap">
         {topForecasts.map((forecast, index) => (
           <div className={mono.className}>
           <div key={index} className="flex gap-2 justify-center items-center">
-            <small>{(index+1).toString().padStart(2, '0')}.</small>
             <p>{forecast.number.toString().padStart(2, '0')}</p>
             <small className="px-1 border border-slate-700 rounded-sm">{forecast.score.toString().padStart(2, '0')}</small>
           </div>
