4ebc64e29df09bf3a3986e4607abaebf29f15383
clodi <ciao@clodi.tech>
2024-04-19 22:28:56 +0200

    read pin setup server action form winners

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -3,6 +3,8 @@ import { sql } from '@vercel/postgres';
 
 const mono = JetBrains_Mono({ subsets: ["latin"], weight: ['300'] });
 
+const pin = process.env.PIN;
+
 function NextLottery({ lottery, forecast, indices}: { lottery: any, forecast: any, indices: number[] }) {
     return (
         <div className='flex flex-col justify-center items-center gap-2 border border-slate-700 rounded-lg py-4'>
@@ -22,6 +24,42 @@ function NextLottery({ lottery, forecast, indices}: { lottery: any, forecast: an
     )
 }
 
+async function handleSubmit(formData: FormData) {
+    "use server"
+    console.log(formData.get('i'));
+    console.log(formData.get('ii'));
+    console.log(formData.get('iii'));
+    console.log(formData.get('iv'));
+    console.log(formData.get('v'));
+    console.log(formData.get('vi'));
+    console.log(formData.get('j'));
+    console.log(formData.get('ss'));
+    console.log(formData.get('pin'));
+}
+
+function FormWinners(){
+    return (
+        <div className={mono.className}>
+            <form action={handleSubmit} className='flex flex-col justify-center items-center gap-4'>
+                <div className='flex gap-1'>
+                    <input name='i' type="text" maxLength={2} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                    <input name='ii' type="text" maxLength={2} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                    <input name='iii' type="text" maxLength={2} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                    <input name='iv' type="text" maxLength={2} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                    <input name='v' type="text" maxLength={2} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                    <input name='vi' type="text" maxLength={2} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                </div>
+                <div className='flex gap-1'>
+                    <input name='j' type="text" maxLength={2} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                    <input name='ss' type="text" maxLength={2} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                    <input name='pin' type="password" maxLength={8} className='h-9 w-11 border-b-2 border-gray-400 bg-transparent px-3 py-1 text-sm focus:border-none focus:outline-none focus:ring-0'/>
+                    <button type="submit" className='h-9 w-11 bg-white text-gray-900 rounded-md'>add</button>
+                </div>
+            </form>
+      </div>
+    )
+}
+
 export default async function Page() {
     // get the forecast id from the next lottery
     const { rows: [next] } = await sql`SELECT * FROM lottery WHERE forecast_id is not null AND winners_id is null`;
@@ -38,6 +76,7 @@ export default async function Page() {
     return(
         <main>
             <NextLottery lottery={next} forecast={forecast} indices={indices} />
+            <FormWinners />
             {JSON.stringify(last)}
         </main>
     )
